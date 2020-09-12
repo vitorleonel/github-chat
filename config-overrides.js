5773ab13b1d3891ddc4de191e79a084825b1020a
Vitor Leonel <falecom@vitorleonel.com>
2020-09-12 02:39:34 -0300

    change webpack configs

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+/* config-overrides.js */
+
+module.exports = function override(config, env) {
+  config.optimization.splitChunks = {
+    cacheGroups: {
+      default: false,
+    },
+  };
+
+  config.optimization.runtimeChunk = false;
+
+  config.output.filename = "static/js/[name].js";
+
+  return config;
+};
